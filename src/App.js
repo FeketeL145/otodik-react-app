4acd7f0238d412243480013cef2b4d8b53ea65a4
FeketeL145 <feketel@kkszki.hu>
2023-11-22 10:10:56 +0100

    otodik-react-app, pizza fetch, useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  const [isFetchPending, setFetchPending] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://pizza.kando-dev.eu/Pizza');
+        const data = await response.json();
+        const processedData = data.map((pizza) => ({
+          id: pizza.id,
+          name: pizza.name,
+          isGlutenFree: pizza.isGlutenFree === 1 ? 'Igen' : 'Nem',
+          imgUrl: pizza.kepURL,
+        }));
+        setPizzas(processedData);
+      } finally {
+        setFetchPending(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='p-2 text-center'>Pizzák</h1>
+      {isFetchPending ? (
+        <div className='text-center'>
+          <div className='spinner-border' role='status'>
+          </div>
+        </div>
+      ) : (
+        <div className='row'>
+          {pizzas.map((pizza) => (
+            <div key={pizza.id} className='col-md-4 mb-4'>
+              <div className='card text-center'>
+                <div className='card-body'>
+                  <p className='card-title'>Pizza neve: {pizza.name}</p>
+                  <h5 className='card-text'>Gluténmentes-e: {pizza.isGlutenFree}</h5>
+                  <img src={pizza.imgUrl} className='card-img-top w-50' alt={pizza.name}/>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
